300188938fe1e889f1f9dccdea2a65bbae378ea4
JumaCodes <olumesestevejohn@gmail.com>
2025-08-16 17:53:03 +0100

    prepare for vercel deploy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,20 +2,18 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
+
+  secret: process.env.NEXTAUTH_SECRET, // 👈 add this
+
   callbacks: {
     async session({ session, token }) {
-      session.user.tag = session.user.name
-        .split(" ")
-        .join("")
-        .toLocaleLowerCase();
+      session.user.tag = session.user.name.split(" ").join("").toLowerCase();
 
       session.user.uid = token.sub;
       return session;
