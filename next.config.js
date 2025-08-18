fe8f4f9f5d32235be7e64ae72e3ad88aea796d8b
JumaCodes <olumesestevejohn@gmail.com>
2025-08-18 23:47:08 +0100

    fixing 500 - internal error vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
   reactStrictMode: true,
   images: {
     remotePatterns: [
